304282505491ca5a632a5ba4f2661de1ad607400
qml <569040804@qq.com>
2020-11-06 17:55:15 +0800

    feign继承特性和日志，因为版本原因没有引入重试机制

--- feign-consumer/src/main/java/com/clear/FeignConsumer.java
+++ feign-consumer/src/main/java/com/clear/FeignConsumer.java
@@ -1,8 +1,10 @@
 package com.clear;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 /**
  * ClassName FeignConsumer
@@ -16,5 +18,24 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class FeignConsumer {
     public static void main(String[] args) {
         SpringApplication.run(FeignConsumer.class, args);
+    }
+
+
+    /**
+     * 使用了 Feign 之后，如果希望能够查看微服务之间调用的日志，则可以通过开启 Feign 的日志功能实现， Feign 中的日志级别一共分为四种：
+     * <p>
+     * NONE ，不开启日志记录，默认即此；
+     * BASIC ，记录请求方法和请求 URL ，以及响应的状态码以及执行时间；
+     * HEADERS ，在第2条的基础上，再增加请求头和响应头；
+     * FULL ，在第3条的基础上再增加 body 以及元数据。
+     *
+     * @param
+     * @return feign.Logger.Level
+     * @author qml
+     * @date 2020/11/6 17:40
+     **/
+    @Bean
+    Logger.Level loggerLevel() {
+        return Logger.Level.FULL;
     }
 }
